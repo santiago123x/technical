7ae2f6f501f620b7b2ab7bab80ae31e88b7bd0a6
Santiago Calderon <santiago.calderon@spellty.com>
2022-08-07 02:43:59 -0500

    add react

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -1,4 +1,4 @@
-import {useState,useContext} from 'react';
+import React,{useState,useContext} from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
